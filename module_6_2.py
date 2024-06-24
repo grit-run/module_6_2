91e9941503de14ec1f414acb007a73544edb5e24
elinsky <dmitry.ilinsky@gmail.com>
2024-06-24 17:02:13 +0300

    attributes changed from protected to private

--- module_6_2.py
+++ module_6_2.py
@@ -1,20 +1,20 @@
 class Vehicle:
-    _COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
     def __init__(self, owner, model, engine_power, color):
         self.owner = owner
         self.model = model
-        self._engine_power = engine_power
-        self._color = color
+        self.__engine_power = engine_power
+        self.__color = color
 
     def get_model(self):
         return "Модель: {}".format(self.model)
 
     def get_engine_power(self):
-        return "Мощность двигателя: {}".format(self._engine_power)
+        return "Мощность двигателя: {}".format(self.__engine_power)
 
     def get_color(self):
-        return "Цвет: {}".format(self._color)
+        return "Цвет: {}".format(self.__color)
 
     def print_info(self):
         print(self.get_model())
@@ -23,8 +23,8 @@ class Vehicle:
         print("Владелец: {}".format(self.owner))
 
     def set_color(self, new_color):
-        if str(new_color).lower() in self._COLOR_VARIANTS:
-            self._color = new_color
+        if str(new_color).lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
         else:
             print("Нельзя сменить цвет на {}".format(new_color))
 
